8f73824237c1d3c9a6537310235ea9afb59a5ba3
王志英 <992388202@qq.com>
2021-01-06 18:06:48 +0800

    简化mybatis拦截器

--- spring-boot-demo/src/main/java/com/wzy/boot/demo/config/MyInterceptor.java
+++ spring-boot-demo/src/main/java/com/wzy/boot/demo/config/MyInterceptor.java
@@ -1,16 +1,16 @@
 package com.wzy.boot.demo.config;
 
-import com.alibaba.fastjson.JSON;
 import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Component;
-
-import java.util.Properties;
 
 /**
  * @author 王志英
@@ -25,14 +25,5 @@ public class MyInterceptor implements Interceptor {
     public Object intercept(Invocation invocation) throws Throwable {
         System.err.println("走入自定义拦截器。。。");
         return invocation.proceed();
-    }
-    @Override
-    public Object plugin(Object target) {
-        System.err.println("自定义拦截器plugin。。");
-        return Plugin.wrap(target, this);
-    }
-    @Override
-    public void setProperties(Properties properties) {
-        System.err.println("自定义拦截器setProperties。。");
     }
 }
